ed5f73d6954b4b9e2f33c720c049b726e8aee46b
Valentin Oszadcsij <valentinosadchiy@gmail.com>
2025-08-22 22:09:29 +0200

    almost nothing

--- src/app/controllers/HomeController.php
+++ src/app/controllers/HomeController.php
@@ -1,9 +1,6 @@
 <?php
 namespace App\Controllers;
 
-require_once __DIR__ . '/../models/User.php';
-
-
 use App\Models\User;
 
 class HomeController
--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,7 @@
 <?php
 // src/public/index.php
 // This is the front controller. All requests are routed here.
-require_once __DIR__ . '/../app/controllers/HomeController.php';
+require_once __DIR__ . '/../core/Autoload.php';
 
 use App\Controllers\HomeController;
 // Instantiate the home controller and call a method
